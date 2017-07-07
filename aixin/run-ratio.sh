520e70779c7102f17a94a12638bee1b93698ade6
Ai Xin <scicomath@gmail.com>
2017-07-07 16:48:06 +0800

    add ratio result

---
+++ aixin/run-ratio.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+./aixin 2 disklike Au 62 0.1 500 > result/ratio/ratioAu62GeV0.1.txt
+./aixin 2 disklike Au 62 0.2 500 > result/ratio/ratioAu62GeV0.2.txt
+./aixin 2 disklike Au 62 0.3 500 > result/ratio/ratioAu62GeV0.3.txt
+./aixin 2 disklike Au 130 0.1 500 > result/ratio/ratioAu130GeV0.1.txt
+./aixin 2 disklike Au 130 0.2 500 > result/ratio/ratioAu130GeV0.2.txt
+./aixin 2 disklike Au 130 0.3 500 > result/ratio/ratioAu130GeV0.3.txt
+./aixin 2 disklike Au 200 0.1 500 > result/ratio/ratioAu200GeV0.1.txt
+./aixin 2 disklike Au 200 0.2 500 > result/ratio/ratioAu200GeV0.2.txt
+./aixin 2 disklike Au 200 0.3 500 > result/ratio/ratioAu200GeV0.3.txt
+./aixin 2 disklike Pb 2760 0.1 500 > result/ratio/ratioPb2760GeV0.1.txt
+./aixin 2 disklike Pb 2760 0.2 500 > result/ratio/ratioPb2760GeV0.2.txt
+./aixin 2 disklike Pb 2760 0.3 500 > result/ratio/ratioPb2760GeV0.3.txt
+./aixin 2 disklike Cu 62 0.1 500 > result/ratio/ratioCu62GeV0.1.txt
+./aixin 2 disklike Cu 62 0.2 500 > result/ratio/ratioCu62GeV0.2.txt
+./aixin 2 disklike Cu 62 0.3 500 > result/ratio/ratioCu62GeV0.3.txt
+./aixin 2 disklike Cu 200 0.1 500 > result/ratio/ratioCu200GeV0.1.txt
+./aixin 2 disklike Cu 200 0.2 500 > result/ratio/ratioCu200GeV0.2.txt
+./aixin 2 disklike Cu 200 0.3 500 > result/ratio/ratioCu200GeV0.3.txt
